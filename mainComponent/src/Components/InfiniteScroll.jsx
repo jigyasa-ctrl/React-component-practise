ca1db29d6dc4f09e66d3be208f5a3b8b4980535d
Jigyasa Upadhyay <jigyasaupadhyay60@gmail.com>
2026-03-22 17:46:26 +0530

    infinite scroll updated

--- mainComponent/src/Components/InfiniteScroll.jsx
+++ mainComponent/src/Components/InfiniteScroll.jsx
@@ -1,52 +1,28 @@
-import { useState, useEffect, useRef } from "react";
 import "./styles.css";
+import { useState, useEffect } from "react";
 
-export default function InfiniteScroll() {
-  const [pageNum, setPageNum] = useState(1);
-  const [hasMore, setHasMore] = useState(true);
-  const [items, setItems] = useState([]);
-  const loaderRef = useRef(null);
-  const url = `https://jsonplaceholder.typicode.com/posts?_page=${pageNum}&_limit=20`;
-
-  const fetchData = async function () {
-    const response = await fetch(url).then((res) => res.json());
-    if (response.length > 0) {
-      setItems((prev) => [...prev, ...response]);
-    } else {
-      setHasMore(false);
-    }
-  };
-
+export default function App() {
+  const [count, setCount] = useState(50);
   useEffect(() => {
-    if (!hasMore) return;
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting) { // when loader intersects with viewport
-          setPageNum((prev) => prev + 1);
-        }
-      },
-      {
-        threshold: 1,
+    const onScroll = function () {
+      if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        setCount((prev) => (prev += 50));
       }
-    );
-    if (loaderRef.current) {
-      observer.observe(loaderRef.current);
-    }
-    return () => {
-      if (loaderRef.current) observer.unobserve(loaderRef.current);
     };
-  }, [hasMore]);
-
-  useEffect(() => {
-    fetchData();
-  }, [pageNum]);
-
+    document.addEventListener("scroll", onScroll);
+  }, [count]);
+  const element = Array(count).fill("abc");
+  element.push();
   return (
-    <div className="App">
-      {items.length > 0 &&
-        items?.map((data, index) => <div key={index}>{data.title}</div>)}
-      {hasMore && <div ref={loaderRef}>Loading...</div>}
-      {!hasMore && <div>No more data</div>}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {element.map((item, index) => (
+        <span>{index}</span>
+      ))}
     </div>
   );
 }
